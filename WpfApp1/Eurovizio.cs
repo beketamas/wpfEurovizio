5233e213d39f9847d0ca791b8e0eff2edd41a55e
beketamas <72067154+beketamas@users.noreply.github.com>
2024-05-24 11:04:06 +0200

    Add files via upload

---
+++ WpfApp1/Eurovizio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WpfApp1
+{
+    class Eurovizio
+    {
+        int ev;
+        string eloado;
+        string cim;
+        int helyezes;
+        int pontszam;
+
+        public Eurovizio(MySqlDataReader olvas)
+        {
+            this.ev = olvas.GetInt32(0);
+            this.eloado = olvas.GetString(1);
+            this.cim = olvas.GetString(2);
+            this.helyezes = olvas.GetInt32(3);
+            this.pontszam = olvas.GetInt32(4);
+        }
+
+        public int Ev { get => ev; set => ev = value; }
+        public string Eloado { get => eloado; set => eloado = value; }
+        public string Cim { get => cim; set => cim = value; }
+        public int Helyezes { get => helyezes; set => helyezes = value; }
+        public int Pontszam { get => pontszam; set => pontszam = value; }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string connection = "datasource=127.0.0.1;port=3306;username=root;password=;database=eurovizio";
+        List<Eurovizio> lista = new();
+        MySqlConnection nem;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            try
+            {
+                nem = new(connection);
+                nem.Open();
+                string lekerdezes = "SELECT ev, eloado, cim, helyezes, pontszam from dal";
+                MySqlCommand parancs = new(lekerdezes, nem);
+                MySqlDataReader reader = parancs.ExecuteReader();
+                while (reader.Read())
+                    lista.Add(new Eurovizio(reader));
+                reader.Close();
+                dgTabla.ItemsSource = lista;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+            btnDarab.Click += (s, e) => lblDarab.Content = $"{lista.Count} db jómadár";
+        }
+    }
+}
